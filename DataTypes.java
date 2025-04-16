5f79d2c0a31195147029becec8436be1542e8b70
technosaran <saransci2006@gmail.com>
2025-04-16 12:49:10 +0530

    cc

---
+++ DataTypes.java
@@ -0,0 +1,29 @@
+public class DataTypes 
+{
+    public static void main(String args[])
+    {
+        float num = 11; // Valid, but it's better to use 'f' suffix (float num = 11f;)
+        
+        float a = 10.11f; // Float: 4 bytes, approx. range ±3.4e38. Must add 'f' or 'F' to indicate float literal
+
+        int num1 = 10; // Integer: 4 bytes, range -2,147,483,648 to 2,147,483,647
+
+        byte by = 117; // Byte: 1 byte, range -128 to 127
+
+        short sh = 121; // Short: 2 bytes, range -32,768 to 32,767
+
+        long lg = 5842L; // Long: 8 bytes, range -9,223,372,036,854,775,808 to 9,223,372,036,854,775,807 (add 'L' or 'l')
+
+        double d = 12; // Double: 8 bytes, approx. range ±1.7e308 (default for decimal values)
+
+        double d1 = 12; // we can store int as well as decimal in long 
+
+        char c = 'k'; // Char: 2 bytes, stores a single Unicode character (0 to 65,535)
+
+        String c1 = "hello world"; 
+
+        boolean b = true; // Boolean: 1 bit (true or false)
+
+        System.out.println(a);
+    }
+}
---
+++ variables.java
@@ -0,0 +1,13 @@
+public class variables 
+{
+ public static void main(String args[])
+ {
+    int a=10;
+    int s=19;
+    //System.out.println(a+s);
+   //System.out.println(s);
+   int result=a+s;
+   System.out.println(result);                                          // result it will work more efficient 
+
+ }    
+}
